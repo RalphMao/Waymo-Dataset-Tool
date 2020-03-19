8ab475aa8e1f3a9be64d9e54634d033f80f9015c
Huizi Mao <ralphmao95@gmail.com>
2020-03-19 12:13:41 -0700

    initial release

---
+++ batch_download.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+import glob
+import os
+import argparse
+from convert_tfrecord import extract_frame, WAYMO_CLASSES
+
+parser = argparse.ArgumentParser()
+parser.add_argument('split', choices=['training', 'validation'])
+parser.add_argument('--out-dir', default='./tmp')
+parser.add_argument('--resize', default=0.5625, type=float)
+args = parser.parse_args()
+
+url_template = 'gs://waymo_open_dataset_v_1_0_0/{split}/{split}_%04d.tar'.format(split=args.split)
+if args.split == 'training':
+    num_segs = 32
+elif args.split == 'validation':
+    num_segs = 8
+
+if not os.path.exists(args.out_dir):
+    os.mkdir(args.out_dir)
+
+clip_id = len(glob.glob('labels/*.txt'))
+for seg_id in range(0, num_segs):
+    flag = os.system('gsutil cp ' + url_template % seg_id + ' ' + args.out_dir)
+    assert flag == 0, 'Failed to download segment %d. Make sure gsutil is installed'%seg_id
+    os.system('cd %s; tar xf %s_%04d.tar'%(args.out_dir, args.split, seg_id))
+    tfrecords = sorted(glob.glob('%s/*.tfrecord'%args.out_dir))
+    for record in tfrecords:
+        extract_frame(record, 'labels/%05d.txt'%clip_id, 'images/%05d'%clip_id, WAYMO_CLASSES, resize_ratio=args.resize)
+        print("Clip %d done"%clip_id)
+        clip_id += 1
+        os.remove(record)
+
+    print("Segment %d done"%seg_id)
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Waymo Open Dataset Download Tool
+
+This tool downloads Waymo Open Dataset and converts it into KITTI-like format that consists of individual images and txt labels.
+
+## Prerequisite
+
+* python 2 or 3
+* gsutil (command line tool to download files from google cloud)
+* waymo_open_dataset (follow the [Quick Start]([https://github.com/waymo-research/waymo-open-dataset/blob/master/docs/quick_start.md](https://github.com/waymo-research/waymo-open-dataset/blob/master/docs/quick_start.md)))
+
+## How to use
+
+Before you run the following code, make sure you have set up `gsutil` and `waymo_open_dataset`! Currently only the original training and validation set are supported. Feel free to modify the code to download other data splits.
+
+```
+python batch_download.py training --out-dir /ssd/dataset/waymo --resize 1.0
+```
